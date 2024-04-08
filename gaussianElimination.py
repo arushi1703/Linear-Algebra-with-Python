cb7add31e7c372405e3a02439529993fbf9d55bb
arushisinghi <127681468+arushi1703@users.noreply.github.com>
2024-04-08 09:16:19 +0530

    Update gaussianElimination.py

--- gaussianElimination.py
+++ gaussianElimination.py
@@ -3,30 +3,31 @@ def matrix_input(m,rows,cols):
         for j in range(cols):
             m[i][j]=int(input(f"m[{i}][{j}]:"))
 
-def gaussElim(m,rows,cols):
+def gaussianElimination(m,rows,cols):
     for i in range(rows):
-        if m[i][i]==0:
+        if m[i][i] == 0.0:
             return "Invalid"
         for j in range(rows):
-            if i!=j:
-                factor=m[j][i]/m[i][i]
+            if i != j:
+                factor = m[j][i] /m[i][i]
                 for k in range(cols):
-                    m[j][k]=m[j][k]- factor*m[i][k]
-    print("Echelon Form:")
+                    m[j][k] = m[j][k] - factor * m[i][k]
+    print("Echelon form:")
     [print(row) for row in m]
-
+    
     for i in range(rows):
-        factor=m[i][i]
+        factor = m[i][i]
         for j in range(cols):
-            m[i][j]=round(m[i][j]/factor)
-
-    print("Row cannonical Form:")
+            m[i][j] = round(m[i][j] / factor)
+    print("Row cannonical form:")
     [print(row) for row in m]
-
+    
+    return [row[-1] for row in m]
+    
 rows=int(input("Enter number of rows:"))
 cols=int(input("Enter number of cols:"))
 print("Enter elements:")
 m=[[0 for j in range(cols)]for i in range(rows)]
 matrix_input(m,rows,cols)
-gaussElim(m,rows,cols)
+print(f"[x,y,z]={gaussianElimination(m,rows,cols)}")
 
